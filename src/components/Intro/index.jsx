d3ba3966b21c3417f0f19e0b45a035fe10a55c2e
Gustavo Cardoso <gustavocardoso@gmail.com>
2018-12-21 15:18:40 -0800

    Change function to class

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { Screen, Title } from '../../styled-components/global'
 import { StartButton } from './style'
 
-const Intro = () => (
-  <Screen className='intro-container'>
-    <Title intro>The Pet Sounds</Title>
-    <StartButton href='/animals'>[ Press here or hit <b>space</b> ]</StartButton>
-  </Screen>
-)
+class Intro extends Component {
+  constructor (props) {
+    super(props)
+    document.addEventListener('keydown', this.handleKeyDown)
+  }
+
+  render () {
+    return (
+      <Screen className='intro-container'>
+        <Title intro>The Pet Sounds</Title>
+        <StartButton href='/animals'>[ Press here or hit <b>space</b> ]</StartButton>
+      </Screen>
+    )
+  }
+
+  handleKeyDown (e) {
+    console.log(e)
+    console.log('Uepa')
+  }
+}
 
 export default Intro
